158ba4e19d84e99a12676cedf8593eca2a9b139b
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-01-22 01:10:57 +0900

    マインクラフトサーバーの自動/手動での再起動に対応

--- images/minecraft/launcher.sh
+++ images/minecraft/launcher.sh
@@ -3,12 +3,14 @@
 close() {
   tmux send -t minecraft ENTER `echo "$STOP_CMD" | fold -w 1 | paste -s -d ' '` ENTER
 
-  while pgrep java > /dev/null; do
-    sleep 0.05
-  done
+  jwait
 
   echo
   exit 0
+}
+
+jwait() {
+  tail --pid=`pgrep java` -f /dev/null
 }
 
 trap 'close' SIGTERM
@@ -16,5 +18,11 @@ trap 'close' SIGTERM
 tmux new -d -s minecraft 'java -Dminecraft@`basename $PWD` -jar "$JAR_PATH" | tee /tmux.log'
 tail -F /tmux.log 2> /dev/null &
 
-tail -f /dev/null &
+{
+  until pgrep java > /dev/null; do
+    sleep 1
+  done
+
+  jwait
+} &
 wait $!
