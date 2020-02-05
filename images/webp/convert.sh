dbade5a77fc44c8089377dc2d4be89e5001077de
YukiLeafX <yukileafx@gmail.com>
2020-02-05 23:43:27 +0900

    gif2webpのバージョンも表示する

--- images/webp/convert.sh
+++ images/webp/convert.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find . -type f \( -iname '*.png' -or -iname '*.jpg' -or -iname '*.jpeg' \) -print0 | xargs -i -0 cwebp -o {}.webp {}
-find . -type f -iname '*.gif' -print0 | xargs -i -0 gif2webp -o {}.webp {}
+find . -type f \( -iname '*.png' -or -iname '*.jpg' -or -iname '*.jpeg' \) -print0 | xargs -0 -I {} cwebp -o {}.webp {}
+find . -type f -iname '*.gif' -print0 | xargs -0 -I {} gif2webp -o {}.webp {}
