0db4970f6e67c57887abb671f2fe9b0236222727
YukiLeafX <yukileafx@gmail.com>
2020-02-20 03:31:00 +0900

    webpのcontainerをキープしない

--- images/webp/convert.sh
+++ images/webp/convert.sh
@@ -1,13 +1,4 @@
 #!/bin/ash
 
-close() {
-  exit 0
-}
-
-trap 'close' SIGTERM
-
 find . -type f \( -iname '*.png' -or -iname '*.jpg' -or -iname '*.jpeg' \) -print0 | xargs -0 -I {} cwebp -o {}.webp {}
 find . -type f -iname '*.gif' -print0 | xargs -0 -I {} gif2webp -o {}.webp {}
-
-tail -f /dev/null &
-wait $!
