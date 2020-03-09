6556ff5aa67ee8397fab187f23b0d73e11bc9b3c
YukiLeafX <yukileafx@gmail.com>
2020-03-10 03:54:52 +0900

    JDKの修正、SIGINTで終了

--- images/minecraft/launcher.sh
+++ images/minecraft/launcher.sh
@@ -1,19 +1,7 @@
 #!/bin/ash
 
-close() {
-  tmux send -t minecraft ENTER $(echo $STOP_CMD | fold -w 1 | paste -s -d ' ') ENTER
-  jwait
-  exit 0
-}
-
-jwait() {
-  tail --pid=$(pgrep java) -f /dev/null
-}
-
-trap 'close' SIGTERM
-
 if [ ! -z $JAR_DOWNLOAD_URL ]; then
-  SIZE=$(curl -fsSI $JAR_DOWNLOAD_URL | grep -i Content-Length | awk '{ print $2 }')
+  SIZE=$(wget -qS --spider $JAR_DOWNLOAD_URL 2> /dev/stdout | grep -i Content-Length | awk '{ print $2 }')
 
   if [ ! -f $JAR_PATH ] || [[ $SIZE != $(stat -c %s $JAR_PATH) ]]; then
     wget -O $JAR_PATH $JAR_DOWNLOAD_URL
@@ -28,12 +16,7 @@ if [ $(echo $(cat /proc/loadavg | cut -d ' ' -f 1) / 1 | bc) -ge $(( $(nproc --a
   sleep $(shuf -n 1 -i 30-$(( 2 * 60 )))
 fi
 
-tmux new -d -s minecraft java $JVM_ARGS -jar $JAR_PATH
-tail -n 0 -F ./proxy.log.0 ./logs/latest.log ./gc.log 2> /dev/null &
+rm -f screenlog.*
+tail -F screenlog.0 2> /dev/null &
 
-until pgrep java > /dev/null; do
-  sleep 1
-done
-
-jwait &
-wait $!
+exec screen -ADmSL minecraft java $JVM_ARGS -jar $JAR_PATH
