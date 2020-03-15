99ed0c6bb27067e7b231ff9ea96d75259efb0dc6
YukiLeafX <yukileafx@gmail.com>
2020-03-15 21:16:20 +0900

    遅れて起動するかを指定可能にする

--- image/minecraft/launcher.sh
+++ image/minecraft/launcher.sh
@@ -24,12 +24,14 @@ if [ ! -z $JAR_DOWNLOAD_URL ]; then
   fi
 fi
 
-if [ $(echo $(cat /proc/uptime | cut -d ' ' -f 1) / 1 | bc) -lt $(( 5 * 60 )) ]; then
-  sleep $(shuf -n 1 -i 30-$(( 2 * 60 )))
-fi
+if [ ! -z $LAZY_STARTUP ] && [[ $LAZY_STARTUP == yes ]]; then
+  if [ $(echo $(cat /proc/uptime | cut -d ' ' -f 1) / 1 | bc) -lt $(( 5 * 60 )) ]; then
+    sleep $(shuf -n 1 -i 30-$(( 2 * 60 )))
+  fi
 
-if [ $(echo $(cat /proc/loadavg | cut -d ' ' -f 1) / 1 | bc) -ge $(( $(nproc --all) / 4 )) ]; then
-  sleep $(shuf -n 1 -i 30-$(( 2 * 60 )))
+  if [ $(echo $(cat /proc/loadavg | cut -d ' ' -f 1) / 1 | bc) -ge $(( $(nproc --all) / 4 )) ]; then
+    sleep $(shuf -n 1 -i 30-$(( 2 * 60 )))
+  fi
 fi
 
 tail -n 0 -F ./proxy.log.0 ./logs/latest.log 2> /dev/null &
