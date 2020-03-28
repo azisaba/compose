a20326a90fd3f18c0205595457e5cc57edb275f0
YukiLeafX <yukileafx@gmail.com>
2020-03-29 02:42:12 +0900

    バックアップのスナップショットをフォルダごとに作成

--- image/backup/backup.sh
+++ image/backup/backup.sh
@@ -11,14 +11,16 @@ chmod 700 ~/.ssh
 cp $SFTP_PRIVATE_KEY ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 
-echo $'Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null' > ~/.ssh/config
+echo $'Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\tLogLevel ERROR' > ~/.ssh/config
 
 for REPO in $(cat $SFTP_HOSTS); do
   export RESTIC_REPOSITORY=$REPO
 
-  if ! restic stats 2> /dev/null; then
+  if ! restic snapshots > /dev/null; then
     restic init
   fi
 
-  restic backup *
+  for TARGET in $(find . -mindepth 1 -maxdepth 1); do
+    restic backup $TARGET
+  done
 done
