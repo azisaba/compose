a105ae3e75801d462231f2921f2c3de6bc5fc06c
YukiLeafX <yukileafx@gmail.com>
2020-04-01 10:55:59 +0900

    shell

--- image/backup/run.sh
+++ image/backup/run.sh
@@ -2,17 +2,17 @@
 
 (
   set -u
-  : $RESTIC_PASSWORD_FILE $SFTP_PRIVATE_KEY $SFTP_HOSTS
+  : "$RESTIC_PASSWORD_FILE" "$SFTP_PRIVATE_KEY" "$SFTP_HOSTS"
 )
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-cp $SFTP_PRIVATE_KEY ~/.ssh/id_ed25519
+cp "$SFTP_PRIVATE_KEY" ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 
 echo $'Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\tLogLevel ERROR' > ~/.ssh/config
 
-for REPO in $(cat $SFTP_HOSTS); do
+for REPO in $(cat "$SFTP_HOSTS"); do
   export RESTIC_REPOSITORY=$REPO
 
   if ! restic snapshots > /dev/null; then
@@ -20,6 +20,6 @@ for REPO in $(cat $SFTP_HOSTS); do
   fi
 
   for TARGET in $(find . -mindepth 1 -maxdepth 1); do
-    restic backup $TARGET
+    restic backup "$TARGET"
   done
 done
