3dd9974658c4cf403a0ac1b591ebfd21e63f8311
YukiLeafX <yukileafx@gmail.com>
2020-04-13 23:22:40 +0900

    定期タスクの修正

---
+++ image/letsencrypt/run-with-cron.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+
+EXEC_LINE="0 */12 * * * cd $PWD && ash /run.sh"
+grep -qxF "$EXEC_LINE" /etc/crontabs/root || echo "$EXEC_LINE" >> /etc/crontabs/root
+
+close() {
+  kill -s SIGTERM "$(pgrep crond)"
+}
+trap 'close' TERM
+crond -f -d 8 &
+wait $!
