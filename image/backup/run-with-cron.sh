12e3338966936a8a711e731c34d975ad891171a0
YukiLeafX <yukileafx@gmail.com>
2020-04-13 23:54:59 +0900

    定期タスクの終了コードを修正

--- image/backup/run-with-cron.sh
+++ image/backup/run-with-cron.sh
@@ -4,8 +4,11 @@ EXEC_LINE="0 */8 * * * cd \"$PWD\" && ash /run.sh"
 grep -qxF "$EXEC_LINE" /etc/crontabs/root || echo "$EXEC_LINE" >> /etc/crontabs/root
 
 close() {
-  kill -s SIGTERM "$(pgrep crond)"
+  kill -SIGTERM "$(pgrep crond)"
+  exit 0
 }
+
 trap 'close' TERM
+
 crond -f -d 8 &
 wait $!
