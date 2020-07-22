96f4af9170847fc5262f4749da2ab15d0d0c4bd2
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-07-23 05:22:38 +0900

    起動時の負荷を83.63%軽減する
    Closes #3

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,16 @@ set -e
 script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 root_dir=$(realpath "$script_dir"/..)
 
+docker_compose_random_up() {
+  for service in $(docker-compose config --services); do
+    (
+      sleep "$(shuf -i 0-300 -n 1)"
+      docker-compose up -d "$service"
+    ) &
+  done
+  wait
+}
+
 (
   cd "$root_dir"
 
@@ -12,5 +22,6 @@ root_dir=$(realpath "$script_dir"/..)
   docker-compose up -d
 
   export COMPOSE_FILE=apps/minecraft/zones.yml:apps/minecraft/restarts.yml:apps/minecraft/compose.yml
-  docker-compose up -d
+  docker-compose up -d mcredis database bungee lobby
+  docker_compose_random_up
 )
