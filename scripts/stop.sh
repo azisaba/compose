700a8da26bef8d38b4205f8ba1eb69019d245b69
YukiLeafX <yukileafx@gmail.com>
2020-08-12 06:46:22 +0900

    fix

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,7 +8,7 @@ root_dir=$(realpath "$script_dir"/..)
 (
   cd "$root_dir"
 
-  export COMPOSE_FILE=apps/web/zones.yml:apps/web/restarts.yml:apps/web/compose.yml
+  export COMPOSE_FILE=web/docker-compose.yml
   docker-compose down
 
   export COMPOSE_FILE=apps/minecraft/zones.yml:apps/minecraft/restarts.yml:apps/minecraft/compose.yml
