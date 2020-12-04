a413b8ff4397a95cfc71dc0984d7ae5cc5d17819
YukiLeafX <yukileafx@gmail.com>
2020-12-05 05:23:38 +0900

    起動時の負荷を軽減

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,9 @@
 set -e
 
 docker-compose build
-docker-compose up -d --scale azisaba-bungee=5
+
+docker-compose config --services | grep -E 'nginx|azisaba-(mariadb|redis|bungee|lobby)' | xargs docker-compose up -d --scale azisaba-bungee=5
+for service in $(docker-compose config --services | grep -vE 'nginx|azisaba-(mariadb|redis|bungee|lobby)'); do
+  ( sleep "$(shuf -i 60-300 -n 1)" && docker-compose up -d "$service" ) &
+done
+wait
